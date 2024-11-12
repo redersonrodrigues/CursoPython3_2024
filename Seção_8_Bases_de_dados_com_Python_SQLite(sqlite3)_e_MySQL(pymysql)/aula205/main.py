2c8820469ee107cbb000d1516b66d6dc6b07df5b
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-11-11 23:03:57 -0300

    Seção 9 aula 207 - django - basico

--- Seção_8_Bases_de_dados_com_Python_SQLite(sqlite3)_e_MySQL(pymysql)/aula205/main.py
+++ Seção_8_Bases_de_dados_com_Python_SQLite(sqlite3)_e_MySQL(pymysql)/aula205/main.py
@@ -2,9 +2,9 @@ import sqlite3
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
-DB_NAME = 'db.sqlite3'
+DB_NAME = "db.sqlite3"
 DB_FILE = ROOT_DIR / DB_NAME
-TABLE_NAME = 'customers'
+TABLE_NAME = "customers"
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
@@ -13,34 +13,29 @@ cursor = connection.cursor()
 # SQL -  INSERT SELECT UPDATE DELETE
 
 # CUIDADO: fazendo delete sem where
-cursor.execute(
-    f'DELETE FROM {TABLE_NAME}'
-)
+# cursor.execute(
+#     f'DELETE FROM {TABLE_NAME}'
+# )
 
 # DELETE mais cuidadoso
-cursor.execute(
-    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
-)
-connection.commit()
+# cursor.execute(
+#     f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+# )
+# connection.commit()
 
 # Cria a tabela
 cursor.execute(
-    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
-    '('
-    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-    'name TEXT,'
-    'weight REAL'
-    ')'
+    f"CREATE TABLE IF NOT EXISTS {TABLE_NAME}"
+    "("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "name TEXT,"
+    "weight REAL"
+    ")"
 )
 connection.commit()
 
 # Registrar valores nas colunas da tabela
-sql = (
-    f'INSERT INTO {TABLE_NAME} '
-    '(name, weight) '
-    'VALUES '
-    '(:nome, :peso)'
-)
+sql = f"INSERT INTO {TABLE_NAME} " "(name, weight) " "VALUES " "(:nome, :peso)"
 # cursor.execute(sql, ['Joana', 4])
 # cursor.executemany(
 #     sql,
@@ -48,39 +43,32 @@ sql = (
 #         ('Joana', 4), ('Luiz', 5)
 #     )
 # )
-cursor.execute(sql, {'nome': 'Sem nome', 'peso': 3})
-cursor.executemany(sql, (
-    {'nome': 'Joãozinho', 'peso': 3},
-    {'nome': 'Maria', 'peso': 2},
-    {'nome': 'Helena', 'peso': 4},
-    {'nome': 'Joana', 'peso': 5},
-))
+cursor.execute(sql, {"nome": "Sem nome", "peso": 3})
+cursor.executemany(
+    sql,
+    (
+        {"nome": "Joãozinho", "peso": 3},
+        {"nome": "Maria", "peso": 2},
+        {"nome": "Helena", "peso": 4},
+        {"nome": "Joana", "peso": 5},
+    ),
+)
 connection.commit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(sql)
 
-    cursor.execute(
-        f'DELETE FROM {TABLE_NAME} '
-        'WHERE id = "3"'
-    )
-    cursor.execute(
-        f'DELETE FROM {TABLE_NAME} '
-        'WHERE id = 1'
-    )
+    cursor.execute(f"DELETE FROM {TABLE_NAME} " 'WHERE id = "3"')
+    cursor.execute(f"DELETE FROM {TABLE_NAME} " "WHERE id = 1")
     connection.commit()
 
     cursor.execute(
-        f'UPDATE {TABLE_NAME} '
-        'SET name="QUALQUER", weight=67.89 '
-        'WHERE id = 2'
+        f"UPDATE {TABLE_NAME} " 'SET name="QUALQUER", weight=67.89 ' "WHERE id = 2"
     )
     connection.commit()
 
-    cursor.execute(
-        f'SELECT * FROM {TABLE_NAME}'
-    )
+    cursor.execute(f"SELECT * FROM {TABLE_NAME}")
 
     for row in cursor.fetchall():
         _id, name, weight = row
